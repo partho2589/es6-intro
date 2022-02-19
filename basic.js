e044a823c561b70fcc8acfd5f92892dc91c22f96
partho2589 <parthomalo615@.com>
2022-02-19 13:10:22 +0600

    added all file

---
+++ basic.js
@@ -0,0 +1,49 @@
+// variable
+var deposit = 400;
+
+//condition 
+if (deposit > 500){
+
+}else if ( deposit < 200){
+
+}
+else if ( deposit == 500){
+
+}
+else if(deposit <= 300){
+
+}else if ( deposit >= 500){
+
+}else if ( deposit != 400){
+
+};
+//  array 
+const number = [20, 30, 40, 44, 444, 444, 111 , 8 ,50];
+const numberCount = number.length;
+number.pop();
+number.push();
+number[2] == 555;
+// check whether 223 included in the array
+if(number.indexOf(223) != -1){
+
+};
+if (number.includes(223)){
+
+};
+
+// loop 
+// while, for
+for ( const number of numbers){
+
+}
+//function 
+function fullName ( first, second){
+    const name = first + second;
+   
+};
+fullName ('partho', 'malo');
+
+//object
+ 
+const studentName ={ name: 'Partho Malo', Roll: 4, Class: 12, subject: 'Computer'
+}
