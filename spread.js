49633477903ed171614919769640ff0b24794402
partho2589 <parthomalo615@.com>
2022-02-20 11:19:41 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,12 @@
+const numbers = [ 10, 50, 44, 90, 99];
+// console.log(numbers)
+// console.log(...numbers)
+
+const max = Math.max(20, 40, 80, 99, 10, 50, 4);
+ 
+const numbers2 = [...numbers];
+numbers.push(55)
+console.log(numbers)
+console.log(numbers2)
+// const maxAllArray = Math.max(...numbers) 
+// console.log(max, maxAllArray )
